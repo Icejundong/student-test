a5d8c507d655eb7f644661fe1cf2d63eae9e138f
ZhangQiang <ZhangQiang@zhiyou100.com>
2016-12-06 10:27:08 +0800

    完成了首页数据展示功能

--- server.js
+++ server.js
@@ -44,5 +44,30 @@ const Student = mongoose.model('students', {
     updateTime: Date
 });
 
+// 处理首页的接口
+app.get('/', (req, res) => {
+    // select对数据属性进行筛选，属性名之间用空格分隔
+    Student.find().select('name isMale age phone email').exec((err, data) => {
+        
+        if(err){
+            //跳转到错误页
+        }
+        else{
+            // data是一个model数组
+            // model.toObject()可以将数据从模型实例中剥离出来
+            // console.dir(data)
+            console.dir(data.map(m => m.toObject()))
+            
+
+            res.render('index', {students: data.map(m => {
+                m = m.toObject()
+                m.id = m._id.toString()
+                delete m._id
+                return m
+            })});
+        }
+    });
+});
+
 
 app.listen(3000, () => console.log('正在运行...'));
